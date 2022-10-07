664bbfc754c74315e754b4d0b5c5849cbfcf94e2
Istvan <istvan.balogh@outlook.com>
2022-10-07 20:41:07 +0200

    Initial Commit

---
+++ ContinuationTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContinuationTest
+//
+//  Created by Istvan Balogh on 07.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
